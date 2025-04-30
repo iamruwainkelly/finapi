3042f3607839a5c5efbe40740fc51bc41ae65804
Lyall <lyall.vanderlinde@iriesoft.net>
2025-04-30 17:54:32 +0200

    excluded some routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,35 +1,36 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { AppService } from './app.service';
-import yahooFinance from 'yahoo-finance2';
-import { chromium, LaunchOptions } from 'playwright';
-import * as cheerio from 'cheerio';
-import * as fs from 'node:fs';
+import { Controller, Get, Param } from "@nestjs/common";
+import { AppService } from "./app.service";
+import yahooFinance from "yahoo-finance2";
+import { chromium, LaunchOptions } from "playwright";
+import * as cheerio from "cheerio";
+import * as fs from "node:fs";
+import { ApiExcludeController, ApiExcludeEndpoint } from "@nestjs/swagger";
 
 const baseUrls = {
-  investing: 'https://za.investing.com/indices/',
-  yahooFinance: 'https://finance.yahoo.com/',
+  investing: "https://za.investing.com/indices/",
+  yahooFinance: "https://finance.yahoo.com/",
 };
 
 const indexes = [
   {
-    yahooFinanceSymbol: '^GSPC',
-    investingSymbol: 'SPX',
-    investingUrlName: 'us-spx-500',
+    yahooFinanceSymbol: "^GSPC",
+    investingSymbol: "SPX",
+    investingUrlName: "us-spx-500",
   },
   {
-    yahooFinanceSymbol: '^IXIC',
-    investingSymbol: 'IXIC',
-    investingUrlName: 'nasdaq-composite',
+    yahooFinanceSymbol: "^IXIC",
+    investingSymbol: "IXIC",
+    investingUrlName: "nasdaq-composite",
   },
   {
-    yahooFinanceSymbol: '^STOXX50E',
-    investingSymbol: 'STOXX50E',
-    investingUrlName: 'eu-stoxx50',
+    yahooFinanceSymbol: "^STOXX50E",
+    investingSymbol: "STOXX50E",
+    investingUrlName: "eu-stoxx50",
   },
   {
-    yahooFinanceSymbol: '^SSMI',
-    investingSymbol: 'SMI',
-    investingUrlName: 'switzerland-20',
+    yahooFinanceSymbol: "^SSMI",
+    investingSymbol: "SMI",
+    investingUrlName: "switzerland-20",
   },
 ];
 
@@ -39,20 +40,20 @@ const options: LaunchOptions = {
   slowMo: 100,
   // set some args to make playwright behave more like a real browser
   args: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-web-security',
-    '--disable-features=IsolateOrigins,site-per-process',
-    '--allow-insecure-localhost',
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-web-security",
+    "--disable-features=IsolateOrigins,site-per-process",
+    "--allow-insecure-localhost",
   ],
-  ignoreDefaultArgs: ['--enable-automation'],
+  ignoreDefaultArgs: ["--enable-automation"],
 };
 
 // create an array of user agents
 const userAgents = [
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-  'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0',
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+  "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0",
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
 ];
 
 const contextOptions = {
@@ -61,18 +62,19 @@ const contextOptions = {
   deviceScaleFactor: 1,
 };
 
-@Controller('api/')
+@Controller("api/")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/')
+  @ApiExcludeEndpoint()
+  @Get("/")
   get(): string {
     return this.appService.getHello();
   }
 
-  @Get('quote/:symbol')
+  @Get("quote/:symbol")
   async quote(@Param() params: any): Promise<object> {
-    console.log('symbol', params.symbol);
+    console.log("symbol", params.symbol);
 
     const results = await yahooFinance.quote(params.symbol);
 
@@ -82,9 +84,9 @@ export class AppController {
   }
 
   // quoteSummary
-  @Get('quote-summary/:symbol')
+  @Get("quote-summary/:symbol")
   async quoteSummary(@Param() params: any): Promise<object> {
-    console.log('symbol', params.symbol);
+    console.log("symbol", params.symbol);
 
     // const results = await yahooFinance.quoteSummary(params.symbol, {
     //   modules: ['financialData', 'summaryDetail'],
@@ -96,24 +98,35 @@ export class AppController {
     return results;
   }
 
-  @Get('search/:symbol')
+  @Get("search/:symbol")
   async search(@Param() params: any): Promise<object> {
-    console.log('symbol', params.symbol);
+    console.log("symbol", params.symbol);
 
     const results = await yahooFinance.search(params.symbol);
 
     return results;
   }
 
-  @Get('marketmovers/:symbol')
+  @Get("market-movers/:index")
   async marketwatch(@Param() params: any): Promise<object> {
+    // throw an error if they did not prefix the index with ^
+    if (!params.index.startsWith("^")) {
+      return {
+        error:
+          "Index should be prefixed with ^. Index should be one of the following: ^GSPC, ^IXIC, ^STOXX50E, ^SSMI",
+      };
+    }
+
     //get the index from the indexes array
     const index = indexes.find(
-      (index) => index.yahooFinanceSymbol === params.symbol,
+      (index) => index.yahooFinanceSymbol === params.index,
     );
 
     if (!index) {
-      return { error: 'Index not found' };
+      return {
+        error:
+          "Index not found. Index should be one of the following: ^GSPC, ^IXIC, ^STOXX50E, ^SSMI",
+      };
     }
 
     const url = `https://za.investing.com/indices/${index.investingUrlName}`;
@@ -123,7 +136,7 @@ export class AppController {
     const context = await browser.newContext(contextOptions);
 
     const page = await context.newPage();
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
     const content = await page.content();
     await browser.close();
 
@@ -133,13 +146,13 @@ export class AppController {
     const gainersTable = $('[data-test="gainers-table"] tbody tr');
     const topGainers = gainersTable
       .map((_, row) => {
-        const cells = $(row).find('td');
+        const cells = $(row).find("td");
         return {
           name: $(cells[0])
             .find('[data-test="gainers-losers-label"]')
             .text()
             .trim(),
-          symbol: $(cells[0]).find('span.font-semibold').text().trim(),
+          symbol: $(cells[0]).find("span.font-semibold").text().trim(),
           price: $(cells[1])
             .find('[data-test="gainers-losers-last"]')
             .text()
@@ -160,13 +173,13 @@ export class AppController {
     const losersTable = $('[data-test="losers-table"] tbody tr');
     const topLosers = losersTable
       .map((_, row) => {
-        const cells = $(row).find('td');
+        const cells = $(row).find("td");
         return {
           name: $(cells[0])
             .find('[data-test="gainers-losers-label"]')
             .text()
             .trim(),
-          symbol: $(cells[0]).find('span.font-semibold').text().trim(),
+          symbol: $(cells[0]).find("span.font-semibold").text().trim(),
           price: $(cells[1])
             .find('[data-test="gainers-losers-last"]')
             .text()
@@ -192,19 +205,20 @@ export class AppController {
   }
 
   // route for scra[ping a url
-  @Get('scrape')
+  @ApiExcludeEndpoint()
+  @Get("scrape")
   async scrape(): Promise<object> {
     const url =
-      'https://markets.businessinsider.com/index/market-movers/s&p_500';
+      "https://markets.businessinsider.com/index/market-movers/s&p_500";
     const browser = await chromium.launch(options);
     const context = await browser.newContext(contextOptions);
     const page = await context.newPage();
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
     const content = await page.content();
     await browser.close();
 
     // save the content to a file
-    fs.writeFileSync('scrape.html', content);
+    fs.writeFileSync("scrape.html", content);
 
     return { content };
   }
@@ -229,6 +243,6 @@ https://priceapi.moneycontrol.com/technicalCompanyData/usMarket/getTopGainer?ind
   - https://gdsapi.cnbc.com/market-mover/groupMover/SP500/CHANGE_PCT/BOTH/12.json?source=SAVED&delayed=false&partnerId=2
     - has more than 5 symbols for top gainers and losers
     - json api
-    
+
 
 */
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('NestJS Yahoo Finance API')
-    .setDescription('The NestJS Yahoo Finance API description')
-    .setVersion('1.0')
-    .addTag('yahoo-finance')
+    .setTitle("NestJS Finance API")
+    .setDescription("The NestJS Finance API description")
+    .setVersion("1.0")
+    .addTag("yahoo-finance")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
